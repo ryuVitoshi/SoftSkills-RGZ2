9bf2e1f0bf40768cf6eaf87be1a515f0a315022c
ryuVitoshi <ryu.vitoshi@gmail.com>
2021-06-20 22:04:45 +0300

    Viktor

--- SoftSkillsRGZ2/SoftSkillsRGZ2.cpp
+++ SoftSkillsRGZ2/SoftSkillsRGZ2.cpp
@@ -1,8 +1,52 @@
 ﻿#include <iostream>
+//#include "Vlad.h"
+#include "Viktor.h"
+#include "windows.h"
 using namespace std;
 
 int main () {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
+	// ввід кількості рядків
+	int n;
+	cout << "Введіть кількість рядків:\n-> ";
+	cin >> n;
+
+	// ввід даних
+	char** strings = new char* [n];
+	cin.ignore();
+	for (int i = 0; i < n; i++) {
+		strings[i] = new char[256];
+		cout << i + 1 << ". ";
+		cin.getline(strings[i], 255);
+	}
+
+	// перевірка рядків на унікальність
+	//compare(strings, n);
+
+	// ввід довжини рядків
+	int outSize;
+	cout << "Введіть довжину рядків:\n-> ";
+	cin >> outSize;
+
+	// скорочення рядків
+	bool reduction = reduce(strings, n, outSize, 0);
+
+	// вивід даних
+	if (reduction) {
+		for (int i = 0; i < n; i++) {
+			cout << i + 1 << ". " << strings[i] << endl;
+		}
+	}
+	else {
+		cout << "Неможливо скоротити рядки, щоб всі були унікальними.\n";
+	}
+
+	for (int i = 0; i < n; i++)
+		delete[] strings[i];
+	delete[] strings;
+	return 0;
 	return 0;
 }
 
